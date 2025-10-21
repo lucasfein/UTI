12fbd5d262fcd5f8e4b1f8e56faf0349cb1adc8d
lucasfein <lucas.fein@stud.uni-frankfurt.de>
2025-10-21 22:57:54 +0200

    plots

--- UTI.r
+++ UTI.r
@@ -8,6 +8,10 @@ library(ggplot2)
 library(dunn.test)
 
 library(ggsignif)
+library(scales)
+library(cowplot)
+
+pdf(width=14)
 
 data1 <- read_excel("UTI Model Bacterial and phage counts.xlsx", sheet = "MM02", range = "B2:E13", col_names = FALSE)
 
@@ -20,10 +24,6 @@ data1 <- data1 %>% rename(name = `...1`, R1 = `...2`, R2 = `...3`, R3 = `...4`) 
 
 data1 <- data1 %>% group_by(name, biological) %>% summarize(value=mean(value), .groups="drop")
 
-ggplot(data1, aes(x=name, y=value)) + stat_summary(aes(y=value), fun=mean, geom="bar") + geom_jitter(aes(shape=factor(biological)), width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) +labs(y="CFU/ml", shape="biological\nreplicate") + theme_bw() + theme(axis.title.x=element_blank())
-
-ggplot(data1, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="CFU/ml") + theme_bw() + theme(axis.title.x=element_blank())
-
 data2 <- read_excel("UTI Model Bacterial and phage counts.xlsx", sheet = "MM02", range = "B21:E36", col_names = FALSE)
 
 data2 <- data2 %>% rename(name = `...1`, R1 = `...2`, R2 = `...3`, R3 = `...4`) %>%
@@ -34,10 +34,6 @@ data2 <- data2 %>% rename(name = `...1`, R1 = `...2`, R2 = `...3`, R3 = `...4`) 
     mutate(biological = as.integer(sub("V([0-9]+)", "\\1", biological)))
 
 data2 <- data2 %>% group_by(name, biological) %>% summarize(value=mean(value), .groups="drop")
-
-ggplot(data2, aes(x=name, y=value)) + stat_summary(aes(y=value), fun=mean, geom="bar") + geom_jitter(aes(shape=factor(biological)), width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="PFU/ml", shape="biological\nreplicate") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 8923 + MM02 3h", end="NC", label="p < 0.01", y=175000), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE))
-
-ggplot(data2, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="PFU/ml") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 8923 + MM02 3h", end="NC", label="p < 0.01", y=175000), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE))
 
 data3 <- read_excel("UTI Model Bacterial and phage counts.xlsx", sheet = "P00", range = "B2:E13", col_names = FALSE)
 
@@ -50,10 +46,6 @@ data3 <- data3 %>% rename(name = `...1`, R1 = `...2`, R2 = `...3`, R3 = `...4`) 
 
 data3 <- data3 %>% group_by(name, biological) %>% summarize(value=mean(value), .groups="drop")
 
-ggplot(data3, aes(x=name, y=value)) + stat_summary(aes(y=value), fun=mean, geom="bar") + geom_jitter(aes(shape=factor(biological)), width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="CFU/ml", shape="biological\nreplicate") + theme_bw() + theme(axis.title.x=element_blank())
-
-ggplot(data3, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + expand_limits(y=0) + labs(y="CFU/ml") + theme_bw() + theme(axis.title.x=element_blank())
-
 data4 <- read_excel("UTI Model Bacterial and phage counts.xlsx", sheet = "P00", range = "B23:E36", col_names = FALSE)
 
 data4 <- data4 %>% rename(name = `...1`, R1 = `...2`, R2 = `...3`, R3 = `...4`) %>%
@@ -65,25 +57,17 @@ data4 <- data4 %>% rename(name = `...1`, R1 = `...2`, R2 = `...3`, R3 = `...4`) 
 
 data4 <- data4 %>% group_by(name, biological) %>% summarize(value=mean(value), .groups="drop")
 
-ggplot(data4, aes(x=name, y=value)) + stat_summary(aes(y=value), fun=mean, geom="bar") + geom_jitter(aes(shape=factor(biological)), width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="PFU/ml", shape="biological\nreplicate") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 7958 + G10400 3h", end="NC", label="p < 0.01", y=3.5e05), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE))
-
-ggplot(data4, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="PFU/ml") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 7958 + G10400 3h", end="NC", label="p < 0.01", y=3.5e05), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE))
-
 data5 <- read_excel("LDH.xlsx", sheet = "LDH 24h", range = "B2:E5", col_names = FALSE)
 
 data5 <- data5 %>% rename(name = `...1`, `LDH V1` = `...2`, `LDH V2` = `...3`, `LDH V3` = `...4`) %>%
     pivot_longer(cols=`LDH V1`:`LDH V3`, names_to="replicate", names_transform=function(name) as.integer(sub("LDH V([0-9]+)", "\\1", name)), values_to="value") %>%
     mutate(name = fct_relevel(fct_recode(name, `UPEC 8923 24h` = "UPEC 8923 24h",  `UPEC 8923 + MM02 24h` = "UPEC 8923 + MM02 24h", `MM02 24h` = "MM02 24h", `Cells only 24h` = "Cells only 24h"), "UPEC 8923 24h", "UPEC 8923 + MM02 24h", "MM02 24h", "Cells only 24h"))
 
-ggplot(data5, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="LDH") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="MM02 24h", end="Cells only 24h", label="p = 0.01", y=150), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE))
-
 data6 <- read_excel("LDH.xlsx", sheet = "LDH 24h", range = "B7:E10", col_names = FALSE)
 
 data6 <- data6 %>% rename(name = `...1`, `LDH V1` = `...2`, `LDH V2` = `...3`, `LDH V3` = `...4`) %>%
     pivot_longer(cols=`LDH V1`:`LDH V3`, names_to="replicate", names_transform=function(name) as.integer(sub("LDH V([0-9]+)", "\\1", name)), values_to="value") %>%
     mutate(name = fct_relevel(fct_recode(name, `UPEC 7958 24h` = "UPEC 7958", `UPEC 7958 + G10400 24h` = "UPEC 7958 + G10400", `G10400 24h` = "G10400 24h", `Cells only 24h` = "Cells only 24h"), "UPEC 7958 24h", "UPEC 7958 + G10400 24h", "G10400 24h", "Cells only 24h"))
-
-ggplot(data6, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_x_discrete(guide = guide_axis(n.dodge = 2)) + labs(y="LDH") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 7958 24h", end="UPEC 7958 + G10400 24h", label="p = 0.01", y=300), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE))
 
 if (kruskal.test(value ~ name, data = data1)$p.value < 0.05) {
     dunn.test(data1 %>% pull(value), data1 %>% pull(name), method="holm", table=FALSE, list=TRUE)
@@ -103,3 +87,18 @@ if (kruskal.test(value ~ name, data = data5)$p.value < 0.05) {
 if (kruskal.test(value ~ name, data = data6)$p.value < 0.05) {
     dunn.test(data6 %>% pull(value), data6 %>% pull(name), method="holm", table=FALSE, list=TRUE)
 }
+
+plot_grid(
+    ggplot(data1, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + labs(y="CFU/mL") + theme_bw() + theme(axis.title.x=element_blank()),
+    ggplot(data3, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + labs(y="CFU/mL") + theme_bw() + theme(axis.title.x=element_blank()),
+    labels=c("A", "B"), align="v")
+
+plot_grid(
+    ggplot(data2, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_y_continuous(labels=scales::label_number_auto()) + labs(y="PFU/mL") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 8923 + MM02 3h", end="NC", label="p < 0.01", y=175000), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE)),
+    ggplot(data4, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + scale_y_continuous(labels=scales::label_number_auto()) + labs(y="PFU/mL") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 7958 + G10400 3h", end="NC", label="p < 0.01", y=3.5e05), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE)),
+    labels=c("A", "B"), align="v")
+
+plot_grid(
+    ggplot(data5, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + labs(y="LDH (U/L)") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="MM02 24h", end="Cells only 24h", label="p = 0.01", y=150), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE)),
+    ggplot(data6, aes(x=name, y=value)) + geom_boxplot() + geom_jitter(width=0.4, height=0) + expand_limits(y=0) + labs(y="LDH (U/L)") + theme_bw() + theme(axis.title.x=element_blank()) + suppressWarnings(geom_signif(data = data.frame(start="UPEC 7958 24h", end="UPEC 7958 + G10400 24h", label="p = 0.01", y=300), aes(xmin = start, xmax = end, annotations = label, y_position=y), manual = TRUE, inherit.aes = FALSE)),
+    labels=c("A", "B"), align="v")
